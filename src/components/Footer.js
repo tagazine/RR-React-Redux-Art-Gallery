9f44c5e6191fb5f473c01bf5f384f67e9f03032a
Your Namegit config --global user.name Your <you@example.com>
2022-06-25 12:44:38 -0400

    Thunk

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Footer() {
+    const mode = useSelector((state) => state.mode)
 
     return (
-        <div className="Footer" style={{'backgroundColor': 'white'}}>
-            <p>Footer Content</p>
+        <div className="Footer" style={{'backgroundColor': mode.color3}}>
+            <p>{ mode.color3 }</p>
         </div>
     )
 }
